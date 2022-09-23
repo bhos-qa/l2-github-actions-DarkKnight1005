ee0b48adc7c86ebbab29be5832e79c950210401e
Ayaz <ayaz.panahov@aih.az>
2022-09-23 22:54:09 +0400

    Updated test

--- app/src/test/java/lab2/AppTest.java
+++ app/src/test/java/lab2/AppTest.java
@@ -6,6 +6,6 @@ import static org.junit.Assert.*;
 public class AppTest {
     @Test public void appHasAGreeting() {
         App classUnderTest = new App();
-        assertNotNull("App must have a greeting", classUnderTest.getGreeting());
+        assertEquals("App must have a greeting which is: Hello World!", "Hello World!", classUnderTest.getGreeting());
     }
 }
