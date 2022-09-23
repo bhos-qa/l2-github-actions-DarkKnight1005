ea15f593a70645aac09bc49185b93c4de9625b83
Ayaz <ayaz.panahov@aih.az>
2022-09-23 23:01:30 +0400

    Fixed failed test

--- app/src/test/java/lab2/AppTest.java
+++ app/src/test/java/lab2/AppTest.java
@@ -6,6 +6,6 @@ import static org.junit.Assert.*;
 public class AppTest {
     @Test public void appHasAGreeting() {
         App classUnderTest = new App();
-        assertEquals("App must have a greeting which is: Hello World!", "Hello World!!", classUnderTest.getGreeting());
+        assertEquals("App must have a greeting which is: Hello World!", "Hello World!", classUnderTest.getGreeting());
     }
 }
